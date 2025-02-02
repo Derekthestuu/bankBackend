a8e560f80dd4c572c217c3a84fc117bd5e054ff9
Derekthestuu <151447046+Derekthestuu@users.noreply.github.com>
2025-02-02 17:51:51 -0500

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ client.query(`
         password VARCHAR(100) NOT NULL,
         firstName VARCHAR(100),
         lastName VARCHAR(100),
-        money DOUBLE DEFAULT 0
+        money DOUBLE PRECISION NOT NULL DEFAULT 0.0
     );
 `)
     .then(() => {
